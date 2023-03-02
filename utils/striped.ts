8cf874fe9b3c194d5ee5690f727630b05db14c91
MrPossumz <100096377+MrPossumz@users.noreply.github.com>
2023-03-01 17:19:57 -0800

    Update striped.ts
    converted to use the deno std dotenv

--- utils/striped.ts
+++ utils/striped.ts
@@ -1,10 +1,12 @@
-import { config } from "dotenv";
+import { load } from "https://deno.land/std@0.178.0/dotenv/mod.ts";
 import Stripe from "stripe";
+
+const env = await load();
 
 const IS_PRODUCTION = Deno.env.get('DENO_ENV') === 'production';
 
-const stripeSEC = (!IS_PRODUCTION ? config().STRIPE_SEC : (Deno.env.get('STRIPE_SEC') as string));
-export const stripePUB = (!IS_PRODUCTION ? config().STRIPE_PUB : (Deno.env.get('STRIPE_PUB') as string));
+const stripeSEC = (!IS_PRODUCTION ? env['STRIPE_SEC'] : (Deno.env.get('STRIPE_SEC') as string));
+export const stripePUB = (!IS_PRODUCTION ? env['STRIPE_PUB'] : (Deno.env.get('STRIPE_PUB') as string));
 
 export const stripe = new Stripe(stripeSEC, {
     httpClient: Stripe.createFetchHttpClient(),
